04f22eab21bd3b1102faa39b36f584d7bc192b8f
hugorgnamien <hugorgnamien@gmail.com>
2022-03-13 09:31:59 -0700

    The files program in language c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Retun: Always 0 (Success)
+ */
+int main(void)
+{
+  int a;
+  char d;
+  long b;
+  float f;
+  printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
+  printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
+  printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+  printf("Size of a long: %lu byte(s)\n", (unsigned long)sizeof(b));
+  return 0;
+}
