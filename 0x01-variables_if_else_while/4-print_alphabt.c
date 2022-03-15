de408739f3e35d6bd64ace014a5bf49f51ab5be7
hugorgnamien <hugorgnamien@gmail.com>
2022-03-14 17:05:21 -0700

    upgrade 0x01 file

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
 /**
  * main - Entry point
  *
@@ -9,16 +8,14 @@
  */
 int main(void)
 {
-  char low, e, q;
-  e = 'e';
-  q = 'q';
-
-  for (low = 'a'; low <= 'z'; low++)
-    {
-      if (low != e && low != q)
-	putchar(low);
-    }
-  putchar('\n');
-
-  return(0);
+char low, e, q;
+e = 'e';
+q = 'q';
+for (low = 'a'; low <= 'z'; low++)
+{
+if (low != e && low != q)
+putchar(low);
+}
+putchar('\n');
+return (0);
 }
--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
 /**
  * main - Entry point
  *
@@ -9,12 +8,9 @@
  */
 int main(void)
 {
-
-  char low;
-
-  for (low = 'z'; low >= 'a'; low--)
-    putchar(low);
-  putchar('\n');
-
-  return (0);
+char low;
+for (low = 'z'; low >= 'a'; low--)
+putchar(low);
+putchar('\n');
+return (0);
 }
