36da77fc47d3ef582312162aa21677e05a9ad97b
hugorgnamien <hugorgnamien@gmail.com>
2022-04-03 00:21:35 +0000

    update

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 /**
  *_abs - Check Holberton
  * @r: An integre input
--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 /**
  * jack_bauer - Check Holberton
  * Description: function prints every minute in one day
