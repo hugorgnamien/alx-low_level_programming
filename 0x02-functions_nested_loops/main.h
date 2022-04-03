fcd4f906bf0b6cde2597594df36d2a4f9f18173f
hugorgnamien <hugorgnamien@gmail.com>
2022-04-03 03:23:06 +0000

    update file

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -11,7 +11,7 @@ int _isupper(int c);
 int add(int a, int b);
 int _isdigit(int c);
 int mul(int a, int b);
-void print_times_table(int n);
+void times_table(void);
 void putformat(int n);
 void print_numbers(void);
 void print_most_numbers(void);
