0dfe7ea9ffa81f7b35c9fb635a88c6aeacec72d3
hugorgnamien <hugorgnamien@gmail.com>
2022-04-03 02:26:34 +0000

    new file

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,7 +4,7 @@
  * Description: It prints the alphabet in lowercase fallowed by a new line
  * Return: Nothing.
  */
-char print_alphabet(void)
+void print_alphabet(void)
 {
 char i;
 for (i = 'a'; i <= 'z'; i++)
