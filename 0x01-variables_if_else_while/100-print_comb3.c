dc703332d439c64164007a46a2a14cfd49a1c2dd
hugorgnamien <hugorgnamien@gmail.com>
2022-05-05 12:57:41 +0000

    update

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,34 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-
+  * main - Prints combination of numbers
+  *
+  * Return: Always (Success)
+  */
 int main(void)
 {
-  int d, p;
-
-  for (d = '0'; d < '9'; d++)
-    {
-      for (d = p + 1; p <= '9'; p++)
-	{
-	  if (p != d)
-	    {
-	      putchar(d);
-	      putchar(p);
-
-	      if (d == '8' && p == '9')
-		continue;
-	      putchar(',');
-	      putchar(' ');
-	    }
-	}
-    }
-  putchar('\n');
-  return (0);
+int c, i;
+for (c = '0'; c <= '9'; c++)
+{
+for (i = '0'; i <= '9'; i++)
+{
+if (c < i)
+{
+putchar(c);
+putchar(i);
+if (c != '8' || (c == '8' && i != '9'))
+{
+putchar(',');
+putchar(' ');
 }
-	       
+}
+}
+}
+putchar('\n');
+return (0);
+}
--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,37 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-
+  * main - Prints 3 combination of numbers
+  *
+  * Return: Always (Success)
+  */
 int main(void)
 {
-  int d, p, q;
-
-  for (d = '9'; d < '9'; d++)
-    {
-      for (p = d+ 1; p <= '9'; p++)
-	{
-	  for (q = p + 1; q <= '9'; q++)
-	    {
-	      if ((p != d) != q)
-		{
-		  putchar(d);
-		  putchar(p);
-		  putchar(q);
-
-		  if (d == '7' && p == '8')
-		    continue;
-		  putchar(',');
-		  putchar(' ');
-		}
-	    }
-	}
-    }
-  putchar('\n');
-  return (0);
+int c, i, k;
+for (c = '0'; c <= '9'; c++)
+{
+for (i = '0'; i <= '9'; i++)
+{
+for (k = '0'; k <= '9'; k++)
+{
+if (c < i && i < k)
+{
+putchar(c);
+putchar(i);
+putchar(k);
+if (c != '7')
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+}
+putchar('\n');
+return (0);
 }
